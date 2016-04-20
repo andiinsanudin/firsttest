ac9f2c352d5fc2226f7e9086d9f115d758632f4d
andiinsanudin <andi@caterpillardev.net>
2016-04-20 11:04:59 +0700

    Add New Activity

---
+++ app/src/main/java/net/caterpillardev/fisrttest/SecondActivity.java
@@ -0,0 +1,16 @@
+package net.caterpillardev.fisrttest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Andi Insanudin on 20/04/2016.
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
